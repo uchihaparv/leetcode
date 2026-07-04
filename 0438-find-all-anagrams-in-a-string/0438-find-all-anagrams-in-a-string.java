4956b885eef8ff12d93792fd32c4d4ddaa83d65e
ParvMahajan <parvmahajan87@gmail.com>
2026-07-04 10:18:49 +0530

    Time: 31 ms (31.34%), Space: 47.4 MB (26.31%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,26 +1,51 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        int[] hash = new int[256];
-        int k = p.length();
-        for(int i = 0; i<p.length(); i++)hash[p.charAt(i)]++;
+
+        if (s.length() < p.length()) {
+            return ans;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
 
         int j = 0;
-        int c = k;
+        int unq = map.size();
 
-        for(int i = 0; i<s.length(); i++){
-            if(hash[s.charAt(i)]>0)c--;
-            hash[s.charAt(i)]--;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-            if(i-j+1 == k){
-                if(c == 0)ans.add(j);
-                if(hash[s.charAt(j)]>=0)c++;
-                hash[s.charAt(j)]++;
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0) {
+                    unq--;
+                }
+            }
+
+            if (i - j + 1 == p.length()) {
+
+                if (unq == 0) {
+                    ans.add(j);
+                }
+
+                char left = s.charAt(j);
+
+                if (map.containsKey(left)) {
+                    map.put(left, map.get(left) + 1);
+                    if (map.get(left) == 1) {   // or > 0
+                        unq++;
+                    }
+                }
+
                 j++;
             }
         }
 
         return ans;
-
     }
 }
