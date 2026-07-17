f05929bbf4cb9a381f76521ae3672c0a3b8ee08e
ParvMahajan <parvmahajan87@gmail.com>
2026-07-17 07:54:28 +0530

    Time: 2 ms (35.34%), Space: 45.7 MB (29.35%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,32 +1,29 @@
 class Solution {
-    public void Permutations(List<List<Integer>> ans, List<Integer> ds, Map<Integer, Boolean> flag, int n,int[]nums){
-        if(ds.size() == n){
+    void backtrack(List<List<Integer>> ans, List<Integer> ds, Map<Integer, Boolean> map, int[] nums, int n, int i){
+        if(i == n){
             ans.add(new ArrayList<>(ds));
             return;
         }
 
-        for(int i = 0; i<n; i++){
-            if(flag.get(nums[i]))continue;
-            flag.put(nums[i],true);
-            ds.add(nums[i]);
-            Permutations(ans,ds,flag,n,nums);
-            flag.put(nums[i],false);
-            ds.remove(ds.size()-1);
-
+        for(int ele : nums){
+            if(!map.get(ele)){
+                map.put(ele,true);
+                ds.add(ele);
+                backtrack(ans,ds,map,nums,n,i+1);
+                ds.remove(ds.size()-1);
+                map.put(ele,false);
+            }
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        int n = nums.length;
+        Map<Integer, Boolean> map = new HashMap<>();
 
-        Map<Integer, Boolean> flag = new HashMap<>();
-        for(int i : nums)
-            flag.put(i,false);
-        
-        Permutations(ans,ds,flag,n,nums);
+        for(int i : nums)map.put(i,false);
+
+        backtrack(ans,ds,map,nums,nums.length,0);
 
         return ans;
-
     }
 }
