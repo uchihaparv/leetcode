25b9473464da42f97b85821f31e381fe32a57989
ParvMahajan <parvmahajan87@gmail.com>
2026-07-17 07:43:05 +0530

    Time: 6 ms (74.63%), Space: 50.3 MB (7.84%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,32 +1,31 @@
 class Solution {
-    public void Sum(List<List<Integer>> ans, List<Integer> ds, int[] candidates, int target, int sum, int n, int i){
-        if(i == n){
+    void backtrack(List<List<Integer>> ans, List<Integer> ds, int[] candidates, int target, int i, int sum){
+        if(i == candidates.length){
             if(sum == target){
                 ans.add(new ArrayList<>(ds));
             }
+
             return;
         }
 
         if(sum+candidates[i]<=target){
             ds.add(candidates[i]);
-            Sum(ans,ds,candidates,target,sum+candidates[i],n,i+1);
+            backtrack(ans,ds,candidates,target,i+1,sum+candidates[i]);
             ds.remove(ds.size()-1);
         }
 
-        int idx = i+1;
+        int idx=i+1;
 
-        while(idx<n && candidates[idx-1] == candidates[idx])idx++;
-        Sum(ans,ds,candidates,target,sum,n,idx);
-    }
+        while(idx<candidates.length && candidates[idx]==candidates[idx-1])idx++;
+        backtrack(ans,ds,candidates,target,idx,sum);
+    } 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        int sum = 0;
         int i = 0;
-        int n = candidates.length;
-
-        Sum(ans,ds,candidates,target,sum,n,i);
+        int sum = 0;
+        backtrack(ans,ds,candidates,target,i,sum);
 
         return ans;
     }
