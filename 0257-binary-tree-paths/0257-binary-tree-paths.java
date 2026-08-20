1177676e898da4b6596f41b38f1545b520eb5812
ParvMahajan <parvmahajan87@gmail.com>
2026-08-20 10:40:01 +0530

    Time: 4 ms (73.05%), Space: 49.8 MB (29.53%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,30 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    void solve(TreeNode root, List<String> ans, String path) {
-
-        if (root == null)
-            return;
-
-        if (path.isEmpty())
-            path = String.valueOf(root.val);
-        else
-            path += "->" + root.val;
-
-        if (root.left == null && root.right == null) {
-            ans.add(path);
-            return;
-        }
-
-        solve(root.left, ans, path);
-        solve(root.right, ans, path);
-    }
-
     public List<String> binaryTreePaths(TreeNode root) {
-
         List<String> ans = new ArrayList<>();
 
-        solve(root, ans, "");
+        backtrack(root , ans, "");
 
         return ans;
+    }
+
+    private void backtrack(TreeNode root, List<String> ans, String path){
+        if(root == null)return;
+
+        if(path.isEmpty()){
+            path = String.valueOf(root.val);
+        }
+        else {
+            path+="->"+root.val;
+        }
+
+        if(root.left == null && root.right == null){
+            ans.add(path);
+        }
+
+        backtrack(root.left, ans , path);
+        backtrack(root.right, ans , path);
     }
 }
