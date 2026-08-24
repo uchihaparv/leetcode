1bbaaa9af8697e54d9bd1220504a5d8447cc1f08
ParvMahajan <parvmahajan87@gmail.com>
2026-08-24 11:49:31 +0530

    Time: 2 ms (34.16%), Space: 45.7 MB (28.89%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,29 @@
 class Solution {
-    void backtrack(List<List<Integer>> ans, List<Integer> ds, Map<Integer, Boolean> map, int[] nums, int n, int i){
-        if(i == n){
+    public void backtrack(List<List<Integer>> ans, List<Integer> ds, Map<Integer, Boolean> map, int i, int[] nums){
+        if(i==nums.length){
             ans.add(new ArrayList<>(ds));
             return;
         }
 
         for(int ele : nums){
             if(!map.get(ele)){
-                map.put(ele,true);
+                map.put(ele, true);
                 ds.add(ele);
-                backtrack(ans,ds,map,nums,n,i+1);
+                backtrack(ans,ds, map,i+1,nums);
                 ds.remove(ds.size()-1);
-                map.put(ele,false);
+                map.put(ele, false);
             }
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
         Map<Integer, Boolean> map = new HashMap<>();
+        for(int ele : nums){
+            map.put(ele,false);
+        }
+        List<Integer> ds = new ArrayList<>();
 
-        for(int i : nums)map.put(i,false);
-
-        backtrack(ans,ds,map,nums,nums.length,0);
+        backtrack(ans,ds,map,0,nums);
 
         return ans;
     }
