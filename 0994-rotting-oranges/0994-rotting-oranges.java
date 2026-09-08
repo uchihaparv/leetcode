3f7d4cb53f19231567259bf4cd4417f74003c35b
ParvMahajan <parvmahajan87@gmail.com>
2026-09-08 22:15:15 +0530

    Time: 2 ms (88.65%), Space: 43.9 MB (84.38%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,73 @@
+class Pair{
+    int i;
+    int j;
+    int t;
+
+    Pair(int i, int j, int t){
+        this.i = i;
+        this.j = j;
+        this.t = t;
+    }
+}
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        boolean[][] v=new boolean[n][m];
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 2){
+                    v[i][j]=true;
+                    q.offer(new Pair(i,j,0));
+                }
+            }
+        }
+
+        int ans=0;
+
+        while(!q.isEmpty()){
+
+            Pair p = q.poll();
+            int i = p.i;
+            int j = p.j;
+            int t = p.t;
+
+            ans = Math.max(ans,t);
+
+            if(i-1>=0 && !v[i-1][j] && grid[i-1][j] == 1){
+                v[i-1][j] = true;
+                q.offer(new Pair(i-1,j,t+1));
+            }
+
+            if(j-1>=0 && !v[i][j-1] && grid[i][j-1] == 1){
+                v[i][j-1] = true;
+                q.offer(new Pair(i,j-1,t+1));
+            }
+
+            if(i+1<n && !v[i+1][j] && grid[i+1][j] == 1){
+                v[i+1][j] = true;
+                q.offer(new Pair(i+1,j,t+1));
+            }
+
+            if(j+1<m && !v[i][j+1] && grid[i][j+1] == 1){
+                v[i][j+1] = true;
+                q.offer(new Pair(i,j+1,t+1));
+            }
+        }       
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 1 && !v[i][j]){
+                    return -1;
+                }
+            }
+        }
+
+        return ans;
+
+    }
+}
