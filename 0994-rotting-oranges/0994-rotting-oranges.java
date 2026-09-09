0dd86b733f55ee97c42761159521c256b0888950
ParvMahajan <parvmahajan87@gmail.com>
2026-09-09 15:25:20 +0530

    Time: 2 ms (88.51%), Space: 44.1 MB (55.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -9,28 +9,25 @@ class Pair{
         this.t = t;
     }
 }
-
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
         Queue<Pair> q = new LinkedList<>();
-        boolean[][] v=new boolean[n][m];
+        boolean[][] v = new boolean[n][m];
 
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(grid[i][j] == 2){
-                    v[i][j]=true;
+                if(grid[i][j] == 2 && !v[i][j]){
+                    v[i][j] = true;
                     q.offer(new Pair(i,j,0));
                 }
             }
         }
 
-        int ans=0;
+        int ans = 0;
 
         while(!q.isEmpty()){
-
             Pair p = q.poll();
             int i = p.i;
             int j = p.j;
@@ -38,27 +35,31 @@ class Solution {
 
             ans = Math.max(ans,t);
 
-            if(i-1>=0 && !v[i-1][j] && grid[i-1][j] == 1){
+            if(i-1>=0 && grid[i-1][j] == 1 && !v[i-1][j]){
                 v[i-1][j] = true;
                 q.offer(new Pair(i-1,j,t+1));
             }
 
-            if(j-1>=0 && !v[i][j-1] && grid[i][j-1] == 1){
+            
+            if(j-1>=0 && grid[i][j-1] == 1 && !v[i][j-1]){
                 v[i][j-1] = true;
                 q.offer(new Pair(i,j-1,t+1));
             }
 
-            if(i+1<n && !v[i+1][j] && grid[i+1][j] == 1){
+            
+            if(i+1<n && grid[i+1][j] == 1 && !v[i+1][j]){
                 v[i+1][j] = true;
                 q.offer(new Pair(i+1,j,t+1));
             }
 
-            if(j+1<m && !v[i][j+1] && grid[i][j+1] == 1){
-                v[i][j+1] = true;
+            
+            if(j+1<m && grid[i][j+1] == 1 && !v[i][j+1]){
+                v[i][j+1]=true;
                 q.offer(new Pair(i,j+1,t+1));
             }
-        }       
+        }
 
+        
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(grid[i][j] == 1 && !v[i][j]){
@@ -68,6 +69,5 @@ class Solution {
         }
 
         return ans;
-
     }
 }
