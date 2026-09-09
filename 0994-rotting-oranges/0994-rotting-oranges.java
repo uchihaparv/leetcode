748bf008c6e66f3477f98fa311ecf4706ec1f6c5
ParvMahajan <parvmahajan87@gmail.com>
2026-09-09 21:44:50 +0530

    Time: 2 ms (88.51%), Space: 44.1 MB (55.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -9,12 +9,16 @@ class Pair{
         this.t = t;
     }
 }
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+
         Queue<Pair> q = new LinkedList<>();
         boolean[][] v = new boolean[n][m];
+
+        int cntFresh = 0;
 
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
@@ -22,11 +26,15 @@ class Solution {
                     v[i][j] = true;
                     q.offer(new Pair(i,j,0));
                 }
+
+                if(grid[i][j] == 1)cntFresh++;
             }
         }
 
+        int cnt = 0;
         int ans = 0;
-
+        int[] dRow = {-1,0,1,0};
+        int[] dCol = {0,1,0,-1};
         while(!q.isEmpty()){
             Pair p = q.poll();
             int i = p.i;
@@ -35,38 +43,19 @@ class Solution {
 
             ans = Math.max(ans,t);
 
-            if(i-1>=0 && grid[i-1][j] == 1 && !v[i-1][j]){
-                v[i-1][j] = true;
-                q.offer(new Pair(i-1,j,t+1));
-            }
+            for(int k = 0;k<4; k++){
+                int nRow = i+dRow[k];
+                int nCol = j+dCol[k];
 
-            
-            if(j-1>=0 && grid[i][j-1] == 1 && !v[i][j-1]){
-                v[i][j-1] = true;
-                q.offer(new Pair(i,j-1,t+1));
-            }
-
-            
-            if(i+1<n && grid[i+1][j] == 1 && !v[i+1][j]){
-                v[i+1][j] = true;
-                q.offer(new Pair(i+1,j,t+1));
-            }
-
-            
-            if(j+1<m && grid[i][j+1] == 1 && !v[i][j+1]){
-                v[i][j+1]=true;
-                q.offer(new Pair(i,j+1,t+1));
-            }
-        }
-
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(grid[i][j] == 1 && !v[i][j]){
-                    return -1;
+                if(nRow>=0 && nCol>=0 && nCol<m && nRow<n && grid[nRow][nCol] == 1 && !v[nRow][nCol]){
+                    q.offer(new Pair(nRow, nCol, t+1));
+                    v[nRow][nCol] = true;
+                    cnt++;
                 }
             }
         }
+
+        if((cnt-cntFresh)!=0)return -1;
 
         return ans;
     }
