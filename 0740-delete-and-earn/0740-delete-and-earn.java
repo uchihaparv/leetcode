0043317540df29f0a19268eff03bb1d78974362f
ParvMahajan <parvmahajan87@gmail.com>
2026-09-15 11:35:22 +0530

    Time: 3 ms (73.8%), Space: 47.1 MB (24.8%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -5,13 +5,12 @@ class Solution {
 
     public int deleteAndEarn(int[] nums) {
 
-        // Find maximum number
+  
         int max = 0;
         for (int num : nums) {
             max = Math.max(max, num);
         }
 
-        // points[i] = total points obtained by taking all i's
         points = new int[max + 1];
 
         for (int num : nums) {
@@ -26,20 +25,18 @@ class Solution {
 
     private int solve(int i) {
 
-        // Base case
+    
         if (i <= 0) {
             return 0;
         }
 
-        // Already calculated
+  
         if (dp[i] != -1) {
             return dp[i];
         }
 
-        // Don't take i
         int notTake = solve(i - 1);
 
-        // Take i -> cannot take i - 1
         int take = points[i] + solve(i - 2);
 
         return dp[i] = Math.max(take, notTake);
